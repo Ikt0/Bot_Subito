c93ca3e305925da3488ae58730d5108150a70600
Ikto <lorenzolimatola@gmail.com>
2024-05-24 22:31:17 +0200

    gitignore edited

--- config_subito.py
+++
@@ -1,7 +0,0 @@
-config={
-    "url_ricerca":"https://www.subito.it/annunci-italia/vendita/usato/?q=steam+deck&from=recentsearches",
-    "intervallo_ricerca":20,
-    "max_prezzo":300,
-    "clear_database_on_startup":"False",
-    "plot_data":"False"
-}
